05c738e55c06094ca729adc360b1d8651c506a47
Andrey Shamis <lolnik@gmail.com>
2013-01-10 15:12:15 +0200

    Added ini file project
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ INIfile/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        INIfile fini = new INIfile("c:\\test1.ini");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       
+            textBox1.Text = fini.getValue("order");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fini.setValue("order", "2000");
+            fini.setValue("newSheet", "WORK VERY VELL");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+          Hashtable d =   fini.getAll();
+
+             foreach (DictionaryEntry entry in d)
+             {
+                 Console.WriteLine("{0}={1}", entry.Key, entry.Value);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ INIfile/WindowsFormsApplication1/WindowsFormsApplication1/INIfile.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using WindowsFormsApplication1;
+
+    class INIfile
+    {
+        private String _iniFileName = "";
+
+        private String _separator = "=*|[]|@*=";
+
+        private Hashtable data = new Hashtable();
+
+        public INIfile(String fName){
+            OpenSettings(fName);
+        }
+        private void OpenSettings(String fileName)
+        {
+            String input = null;
+            _iniFileName = fileName;
+            data.Clear();
+            // create a writer and open the file
+            try
+            {
+                TextReader tw = new StreamReader(fileName);
+                while ((input = tw.ReadLine()) != null)
+                {
+                    if (input.Length > 3)
+                    {
+                        if (input[0] != '#' && input[0] != ';')
+                        {
+                            String[] result = input.Split(new String[] {  _separator}, StringSplitOptions.None);
+                           
+                            data.Add(result[0].Trim(), result[1].Trim());
+                        }
+                    }
+                }
+                // close the stream
+                tw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                SaveSettings(_iniFileName);
+                OpenSettings(_iniFileName);
+
+            }
+        }
+
+
+        public Hashtable getAll()
+        {
+           // ArrayList _list = null;
+           // foreach (DictionaryEntry entry in data)
+           // {
+           //     Console.WriteLine("{0} = {1}", entry.Key, entry.Value);
+            //}
+           // return _list;
+            Hashtable _ret = new Hashtable(data);
+            return (_ret);
+        }
+
+        private void SaveSettings(String fileName)
+        {
+            // create a writer and open the file
+            TextWriter tw = new StreamWriter(fileName);
+
+            foreach (DictionaryEntry de in data){
+                tw.WriteLine("{0}{1}{2}", de.Key, _separator, de.Value);
+            }
+            // close the stream
+            tw.Close();
+        }
+
+        public String getValue(String key)
+        {
+            if (data.ContainsKey(key))
+            {
+                return (data[key].ToString());
+            }
+            return ("");
+        }
+
+        public bool setValue(String key,String val)
+        {
+            if (data.ContainsKey(key)) {
+                data[key] = val;
+            } else{
+                data.Add(key, val);
+            }
+            SaveSettings(_iniFileName);
+            return (true);
+        }
+
+        private String _getKey(String line)
+        {
+            String[] result = line.Split(new Char[] { '=' });
+            return result[0].Trim(); 
+        }
+
+        private String _getValue(String line)
+        {
+            String[] result = line.Split(new Char[] { '=' });
+            return result[1].Trim(); 
+        }
+    }
